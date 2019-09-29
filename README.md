59e7c2d848ec3b198a0e7cd1bc6bddff18beb3a5
BigMurry <hhdiao@siom.ac.cn>
2019-09-29 15:10:51 +0800

    first impl

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # useEtherProvider
 ethers provider with react hooks api
+
+# Prepare
+```
+yarn add ethers react react-dom useEtherProvider
+```
+
+# Example
+```javascript
+import * as ethers from 'ethers';
+import {EtherProvider, useEtherProvider, useAccount} from 'useEtherProvider';
+
+// USDT for example
+const USDT = {
+  abi: [
+    'function transfer(address to, uint value) public',
+    'function balanceOf(address who) public view returns (uint)'
+  ],
+  address: '0xdac17f958d2ee523a2206206994597c13d831ec7'
+};
+
+async function handleTransfer(erc20, to, amount) {
+  const res = await erc20.transfer(to, amount);
+  return res;
+}
+
+const MyApp = () => {
+  const etherProvider = useEtherProvider();
+  const myAddress = useAccount(etherProvider);
+
+  const [usdtContract, setUsdtContract] = useState(null);
+  const [myBalance, setMyBalance] = useState('0.0');
+
+  useEffect(() => {
+    if (etherProvider) {
+      const contract = new ethers.Contract(USDT.address, USDT.abi, etherProvider);
+      setUsdtContract(contract);
+
+      if (myAddress) {
+        contract.balanceOf(myAddress).then(balance => setMyBalance(balance));
+      }
+    }
+  }, [myAddress, etherProvider]);
+
+  const oneUSD = '1000000';
+  const to = '0x123ddd...';
+
+  return (
+    <div>
+      <div>{myBalance}</div>
+      <button onClick={e => handleTransfer(usdtContract, to, oneUSD)}></button>
+    </div>
+  );
+};
+
+const polling = 10000; // 10s polling interval
+let App = () => {
+  return (
+    <EtherProvider networks={[1, 3, 5]} backupJsonRpcUrl={'http://localhost:8485'} ms={polling}>
+      <MyApp />
+    </EtherProvider>
+  );
+};
+```
---
+++ index.js
@@ -0,0 +1,179 @@
+import React, {useState, useEffect} from 'react';
+import * as ethers from 'ethers';
+import _get from 'lodash/get';
+
+const globalStore = {
+  rawWeb3: null,
+  web3: null,
+  jsonRpcUrl: null,
+  jsonRpc: null
+};
+
+function denyMetamask() {
+  const expire = window.localStorage.getItem('metamask-deny');
+  return new Date(expire) > new Date();
+}
+
+function setDenyMetamask() {
+  let d = new Date();
+  d.setHours(d.getHours() - 12);
+  window.localStorage.setItem('metamask-deny', d);
+}
+
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1102.md
+// https://github.com/MetaMask/metamask-extension/issues/5715
+let enabled = false;
+export async function tryEnableWeb3(forceEnable) {
+  try {
+    if (
+      window &&
+      window.ethereum &&
+      (forceEnable || (!enabled && !denyMetamask()))
+    ) {
+      if (window.ethereum.enable) {
+        await window.ethereum.enable();
+        enabled = true;
+      } else if (window.ethereum.sendAsync) {
+        const accounts = await window.ethereum.sendAsync('eth_requestAccounts');
+        if (accounts && accounts.length > 0) {
+          enabled = true;
+        }
+      } else {
+        const accounts = await window.ethereum.send('eth_requestAccounts');
+        if (accounts && accounts.length > 0) {
+          enabled = true;
+        }
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    setDenyMetamask();
+    enabled = false;
+  }
+  return enabled;
+}
+
+function setProvider(jsonRpcUrl) {
+  if (typeof window === 'undefined') {
+    return null;
+  }
+  const current = _get(window, ['ethereum'], _get(window, ['web3', 'currentProvider'], null));
+
+  // create browser web3 provider
+  if (!globalStore.web3 && current && current !== globalStore.rawWeb3) {
+    globalStore.web3 = new ethers.providers.Web3Provider(current);
+  }
+
+  // set web3 raw provider
+  globalStore.rawWeb3 = current;
+
+  // create JsonRpc web3 provider
+  if (!globalStore.jsonRpc && jsonRpcUrl && jsonRpcUrl !== globalStore.jsonRpcUrl) {
+    globalStore.jsonRpc = new ethers.providers.JsonRpcProvider(jsonRpcUrl);
+  }
+
+  globalStore.jsonRpcUrl = jsonRpcUrl;
+}
+
+function getProvider() {
+  return globalStore.web3 || globalStore.jsonRpc;
+}
+
+async function activeProvider(networks = [1, 3, 4, 5], backupJsonRpcUrl) {
+  setProvider(backupJsonRpcUrl);
+  const provd = getProvider();
+  let ok = false;
+  if (provd) {
+    ok = true;
+    const network = parseInt(await provd.getNetwork(), 10);
+    if (~networks.indexOf(network)) {
+      ok = await tryEnableWeb3();
+    }
+  }
+  return ok;
+}
+
+function sleep(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve('schedule'), ms);
+  });
+}
+
+function retry(fn, times, delay) {
+  return async (...args) => {
+    let tried = 0;
+    let ok = false;
+    let err;
+    while (tried <= times) {
+      try {
+        ok = await fn(...args);
+        if (ok) {
+          break;
+        }
+      } catch (e) {
+        ok = false;
+        err = e;
+      }
+      ++tried;
+      await sleep(delay);
+    }
+    return {ok, err};
+  };
+}
+
+const EtherContext = React.createContext();
+const retryActiveProvider = retry(activeProvider, 5, 1000);
+
+export const EtherProvider = function({networks, backupJsonRpcUrl, ms, children}) {
+  const [provider, setProvider] = useState(null);
+  const updateProvider = async () => {
+    await retryActiveProvider(networks, backupJsonRpcUrl);
+    const currentProvider = getProvider();
+    if (currentProvider && currentProvider !== provider) {
+      setProvider(currentProvider);
+    }
+  };
+
+  useEffect(() => {
+    let interval;
+    updateProvider().then(() => {
+      let running = false;
+      interval = setInterval(() => {
+        if (!running) {
+          running = true;
+          updateProvider().then(() => {
+            running = false;
+          }).catch(e => { running = false; });
+        }
+      }, ms);
+    });
+    return () => {
+      clearInterval(interval);
+    };
+  }, [networks, backupJsonRpcUrl]);
+
+  return (
+    <EtherContext.Provider value={provider}>
+      {children}
+    </EtherContext.Provider>
+  );
+};
+
+export function useEtherProvider() {
+  return React.useContext(EtherContext);
+}
+
+export function useAccount(provider) {
+  const [account, setAccount] = useState('');
+  useEffect(() => {
+    if (provider && provider.getSigner) {
+      const signer = provider.getSigner();
+      signer.getAddress().then(add => {
+        if (add && add !== '0x0000000000000000000000000000000000000000') {
+          setAccount(add);
+        }
+      }).catch(e => {});
+    }
+  }, [provider]);
+  return account;
+}
